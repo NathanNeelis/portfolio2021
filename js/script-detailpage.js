4bb2368b2f0b87790d0f0cc5b79e6e756747c814
NathanNeelis <nathan.neelis@hva.nl>
2021-03-17 21:51:54 +0100

    fixed menu bugs, started with design on detail page

--- js/script-detailpage.js
+++ js/script-detailpage.js
@@ -25,3 +25,27 @@ function nextImage() {
 
     menuImage.src = '../images/' + menuIcons[currentPos];
 }
+
+
+// show and hide submenu
+//web
+const projectsWeb = document.getElementById('projectWeb');
+const menuProjectsWeb = document.getElementById('menuProjectWeb')
+const triangleWeb = document.querySelector('.triangle')
+projectsWeb.addEventListener('click', subMenuWebToggle)
+
+//ux
+const projectsUX = document.getElementById('projectUX');
+const menuProjectsUX = document.getElementById('menuProjectUX');
+const triangleUX = document.querySelector('.triangleUX');
+projectsUX.addEventListener('click', subMenuUXToggle);
+
+function subMenuWebToggle() {
+    menuProjectsWeb.classList.toggle('active');
+    triangleWeb.classList.toggle('turn');
+}
+
+function subMenuUXToggle() {
+    menuProjectsUX.classList.toggle('active');
+    triangleUX.classList.toggle('turn');
+}
